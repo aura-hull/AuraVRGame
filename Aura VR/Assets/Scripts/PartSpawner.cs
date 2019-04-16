4021fc73d95d254892df25a75398d15c28b059c1
Liam <azurepheonix@hotmail.co.uk>
2019-04-17 00:22:51 +0100

    Liam// made building of interest prefab

---
+++ Aura VR/Assets/Scripts/PartSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float respawnTimer;
+    [SerializeField]
+    private GameObject spawnPoint;
+    private float timeSinceLastPickup;
+    private PowerState powerState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerState = gameObject.GetComponent<PowerState>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!spawnPoint.activeSelf)
+        {
+            timeSinceLastPickup += Time.deltaTime;
+            if (timeSinceLastPickup > respawnTimer)
+            {
+                spawnPoint.SetActive(true);
+            }            
+        }
+    }
+    public void itemPickedUp()
+    {
+        timeSinceLastPickup = 0;
+    }
+}
---
+++ Aura VR/Assets/Scripts/TextPopUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPopUp : MonoBehaviour
+{
+    [SerializeField]
+    GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        text.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        text.SetActive(false);
+    }
+}
