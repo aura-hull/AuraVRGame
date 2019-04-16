0af89893d4ca0d151086245a78f54ecdfa0e5fd2
Liam <azurepheonix@hotmail.co.uk>
2019-04-17 00:55:38 +0100

    Liam// added power manager and windturbine prefab

---
+++ Aura VR/Assets/PowerManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerManager : MonoBehaviour
+{
+    [SerializeField]
+    float powerOutput=0;
+    [SerializeField]
+    float powerUsage = 0;
+    public static PowerManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreasePowerOutput(float amountToIncrease)
+    {
+        powerOutput += amountToIncrease;
+    }
+    public void IncreasePowerUsage(float usage)
+    {
+        powerUsage += usage;
+    }
+}
---
+++ Aura VR/Assets/PowerOutput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerOutput : MonoBehaviour
+{
+    [SerializeField]
+    float powerOutput = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PowerManager.instance.IncreasePowerOutput(powerOutput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
