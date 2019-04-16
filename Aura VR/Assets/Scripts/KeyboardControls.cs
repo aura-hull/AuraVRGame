4fe8ebd56aee356008b086a3e31accbb2aab0067
William Taylor <willtay123@hotmail.co.uk>
2019-04-16 23:40:50 +0100

    Added items for project setup

---
+++ Aura VR/Assets/Scripts/KeyboardControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardControls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TitanMovement moveComp = this.GetComponent<TitanMovement>();
+
+        if (Input.GetKey(KeyCode.W)) // Move forward
+            moveComp.MoveVector = new Vector3(10, 0, 0);
+        else if (Input.GetKey(KeyCode.S)) // Move backwards
+            moveComp.MoveVector = new Vector3(-10, 0, 0);
+        else if (Input.GetKey(KeyCode.A)) // Move left
+            moveComp.MoveVector = new Vector3(0, 0, 10);
+        else if (Input.GetKey(KeyCode.D)) // Move right
+            moveComp.MoveVector = new Vector3(0, 0, -10);
+        else // Dont move
+            moveComp.MoveVector = new Vector3(0, 0, 0);
+    }
+}
---
+++ Aura VR/Assets/Scripts/TitanMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitanMovement : MonoBehaviour
+{
+    private bool IsAllowedToMove { get; set; }
+    [SerializeField]
+    private float _maxSpeed = 10f;
+
+    // Used for move direction and speed
+    public Vector3 MoveVector { get; set; }
+
+    private Rigidbody rbody; // local store of titans rigidbody
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsAllowedToMove = true;
+        rbody = null;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsAllowedToMove)
+        {
+            // Ensure local rbody store
+            if (rbody == null)
+                rbody = this.GetComponent<Rigidbody>();
+
+            // VR controls
+            InputProcess();
+
+            // Apply max speed
+            if (MoveVector.magnitude > _maxSpeed)
+                MoveVector = MoveVector.normalized * _maxSpeed;
+
+            // Update Velocity
+            rbody.velocity = MoveVector;
+        }
+    }
+
+    void InputProcess()
+    {
+        // Left controller connected
+        if (OVRInput.IsControllerConnected(OVRInput.Controller.LTrackedRemote))
+        {
+            // Thumbstick position
+            Vector2 primaryThumb = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+            primaryThumb *= 10;
+            MoveVector = new Vector3(primaryThumb.x, 0, primaryThumb.y); // apply to movement
+        }
+    }
+}
