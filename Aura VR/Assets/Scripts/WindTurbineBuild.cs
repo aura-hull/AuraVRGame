35d8507e391af9dd7668afd2f08044bbe5b9945e
Liam <azurepheonix@hotmail.co.uk>
2019-05-04 16:31:27 +0100

    Liam// added prefabs
    added oculus prefabs for grabber and started on buildsite prefab

---
+++ Aura VR/Assets/Scripts/WindTurbineBuild.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindTurbineBuild : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> _realBuildParts;
+    [SerializeField]
+    List<GameObject> _holoBuildParts;
+    [SerializeField]
+    GameObject _objectToCreate;
+    int _numberOfBuiltParts = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject part in _holoBuildParts)
+        {
+            if (other.gameObject.name == part.name && part.activeSelf == true)
+            {
+                part.SetActive(false);
+                Destroy(other.gameObject);
+                foreach(GameObject realPart in _realBuildParts)
+                {
+                    if (realPart.name == part.name)
+                    {
+                        realPart.SetActive(true);
+                        _numberOfBuiltParts++;
+                        if (_numberOfBuiltParts >= _realBuildParts.Count)
+                        {
+                            Instantiate(_objectToCreate);
+                            Destroy(gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
