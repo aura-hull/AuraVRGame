06467b5411529dad5435a2a70cec8894e300957c
William Taylor <willtay123@hotmail.co.uk>
2019-05-06 19:20:49 +0100

    added BuildingSite.cs added BuildPart.cs added a materials folder added holo material added a standard material to be used for parts added a holographic shader from the asset store
    Changed BuildSite to use the generalised BuildingSite.cs

---
+++ Aura VR/Assets/Scripts/BuildPart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPart : MonoBehaviour
+{
+    [SerializeField]
+    private string _partName;
+    [SerializeField]
+    private int _numOfUsesLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_partName == null)
+            _partName = "Name Not Given";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string GetName()
+    {
+        return _partName;
+    }
+
+    public void Use()
+    {
+        _numOfUsesLeft--;
+        if (_numOfUsesLeft <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
