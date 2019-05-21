e21c53c073ce36524064fa5edd4c5f4b6e1354c1
William Taylor <willtay123@hotmail.co.uk>
2019-05-21 15:48:04 +0100

    changed power output to be easily scaled based off a distance and an output

--- Aura VR/Assets/Scripts/PowerOutput.cs
+++ Aura VR/Assets/Scripts/PowerOutput.cs
@@ -5,19 +5,40 @@ using UnityEngine;
 public class PowerOutput : MonoBehaviour
 {
     [SerializeField]
-    float _powerOutput = 10;
+    private Vector2 _powerOutputMinMax = new Vector2(10, 100);
     [SerializeField]
-    float _maxDistanceMultiplier = 90;
+    private Vector2 _distanceMinMax = new Vector2(1, 100);
+    public Transform _positionToScaleFrom;
+
     // Start is called before the first frame update
     void Start()
     {
-        PowerManager.Instance.IncreasePowerOutput(_powerOutput);
-        _powerOutput = _powerOutput-(_maxDistanceMultiplier * transform.position.x/150);
+        float distance = Vector3.Distance(_positionToScaleFrom.position, transform.position);
+        distance = Mathf.Clamp(distance, _distanceMinMax.x, _distanceMinMax.y);
+        float scale = MapValue(distance, _distanceMinMax.x, _distanceMinMax.y, 0, 1);
+
+        float powerOutput = Mathf.Lerp(_powerOutputMinMax.x, _powerOutputMinMax.y, scale);
+        PowerManager.Instance.IncreasePowerOutput(powerOutput);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private float MapValue(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        float fromAbs = from - fromMin;
+        float fromMaxAbs = fromMax - fromMin;
+
+        float normal = fromAbs / fromMaxAbs;
+
+        float toMaxAbs = toMax - toMin;
+        float toAbs = toMaxAbs * normal;
+
+        float to = toAbs + toMin;
+
+        return to;
     }
 }
