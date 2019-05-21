584ee076a5e1198a8d1c67a2332cb9af7afde955
William Taylor <willtay123@hotmail.co.uk>
2019-05-21 17:29:38 +0100

    GameManager now tracks play time and will cause the end of the game

--- Aura VR/Assets/Scripts/GameManager.cs
+++ Aura VR/Assets/Scripts/GameManager.cs
@@ -9,6 +9,11 @@ public class GameManager : MonoBehaviour
     private PowerManager _powerManager;
     private ScoreManager _scoreManager;
 
+    [SerializeField]
+    private float _playDurationLimit = 30;
+    private float _playDuration = 0;
+    private bool _playHasStarted = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +29,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_playHasStarted)
+        {
+            _playDuration += Time.deltaTime;
+
+            if (_playDuration >= _playDurationLimit)
+            {
+                // Get final values
+                float finalScore = _scoreManager.Score;
+                float finalNetPower = _powerManager.PowerProduced - _powerManager.PowerUsed;
+                
+                // Game should end
+            }
+        }
+    }
+
+    public void StartPlay()
+    {
+        _playHasStarted = true;
     }
 }
