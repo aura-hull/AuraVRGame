2eb300459b748a0261d5930971da9b9ef1471b4c
h <h>
2019-05-21 13:08:16 +0100

    Powermanager needs to inherit from mono behaviour

--- Aura VR/Assets/Scripts/PowerManager.cs
+++ Aura VR/Assets/Scripts/PowerManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerManager
+public class PowerManager : MonoBehaviour
 {
     private static PowerManager _instance;
     public static PowerManager Instance
