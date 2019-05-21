9c57a92a466fe4f97cf050922a131d506602ec1b
h <h>
2019-05-21 13:02:28 +0100

    scaled power produced based on distance

--- Aura VR/Assets/Scripts/PowerOutput.cs
+++ Aura VR/Assets/Scripts/PowerOutput.cs
@@ -6,10 +6,13 @@ public class PowerOutput : MonoBehaviour
 {
     [SerializeField]
     float _powerOutput = 10;
+    [SerializeField]
+    float _maxDistanceMultiplier = 90;
     // Start is called before the first frame update
     void Start()
     {
         PowerManager.Instance.IncreasePowerOutput(_powerOutput);
+        _powerOutput = _powerOutput-(_maxDistanceMultiplier * transform.position.x/150);
     }
 
     // Update is called once per frame
