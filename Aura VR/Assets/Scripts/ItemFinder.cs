0a0fea7e002708b9db9d302218f809c9a0cbda33
William Taylor <willtay123@hotmail.co.uk>
2019-05-21 20:12:21 +0100

    Added the ItemFinder script to find all build parts in a radius around a given point and return a list of them

---
+++ Aura VR/Assets/Scripts/ItemFinder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemFinder
+{
+    public static List<GameObject> FindItems(Transform scanTransform, float scanRadius)
+    {
+        GameObject[] items = GameObject.FindGameObjectsWithTag("BuildPart");
+
+        List<GameObject> itemsInRange = new List<GameObject>();
+
+        for (int i = 0; i < items.Length; i += 1)
+        {
+            // Check range
+            Transform itemTransform = items[i].transform;
+            Vector2 itemPosition = new Vector2(itemTransform.position.x, itemTransform.position.z);
+            Vector2 scanPosition = new Vector2(scanTransform.position.x, scanTransform.position.z);
+
+            float distance = Vector2.Distance(scanPosition, itemPosition);
+
+            if (distance <= scanRadius)
+            {
+                itemsInRange.Add(items[i]);
+            }
+        }
+
+        return itemsInRange;
+    }
+}
