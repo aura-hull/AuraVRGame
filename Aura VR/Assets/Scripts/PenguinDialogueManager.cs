76c278a1412c3866d9b843031f36b72a968806a6
h <h>
2019-05-21 14:05:27 +0100

    Added animations for penguins to guide player

---
+++ Aura VR/Assets/Scripts/PenguinDialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinDialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    Animator _penguinAnim;
+    [SerializeField]
+    GameObject _penguinModel;
+    [SerializeField]
+    GameObject _auraModel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoToAuraCenter(string id)
+    {
+        _penguinAnim.SetBool(id,true);
+    }
+
+}
