8b6a27c46254417d5f5b14f6e85662c2ff051d74
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-05-23 14:51:19 +0100

    Added world boundaries and a way to position an avatar for VR.

---
+++ Aura VR/Assets/Scripts/Liam Wilson/Gameplay/Boundary.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace AuraHull.AuraVRGame
+{
+    public class Boundary : MonoBehaviour
+    {
+        public static float MIN_DISTANCE = 20.0f;
+
+        private Material _materialInstance;
+        private float sinceLastUpdateVisual = 0.0f;
+        private float lastDistance = MIN_DISTANCE;
+
+        void Awake()
+        {
+            _materialInstance = GetComponent<MeshRenderer>().material;
+        }
+
+        void Update()
+        {
+            sinceLastUpdateVisual += Time.deltaTime;
+            if (lastDistance < MIN_DISTANCE && sinceLastUpdateVisual >= 0.1f)
+            {
+                // Reset boundary color
+                UpdateVisual(MIN_DISTANCE);
+            }
+        }
+
+        public void UpdateVisual(float distance)
+        {
+            if (_materialInstance == null)
+            {
+                return;
+            }
+
+            Color color = _materialInstance.GetColor("_Color");
+            color.a = 1.0f - Mathf.Clamp01(distance / MIN_DISTANCE);
+            _materialInstance.SetColor("_Color", color);
+
+            sinceLastUpdateVisual = 0.0f;
+            lastDistance = distance;
+        }
+    }
+}
---
+++ Aura VR/Assets/Scripts/Liam Wilson/Gameplay/BoundaryChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+
+namespace AuraHull.AuraVRGame
+{
+    public class BoundaryChecker : MonoBehaviour
+    {
+        private static Vector3[] checkDirs =
+        {
+            Vector3.forward, Vector3.back,
+            Vector3.left, Vector3.right
+        };
+
+        void FixedUpdate()
+        {
+            int checkMask = LayerMask.GetMask("WorldBoundaries");
+            RaycastHit info;
+
+            foreach (Vector3 dir in checkDirs)
+            {
+                if (Physics.Raycast(transform.position, dir, out info, Boundary.MIN_DISTANCE, checkMask))
+                {
+                    Boundary b = info.transform.GetComponent<Boundary>();
+
+                    if (b != null)
+                    {
+                        b.UpdateVisual(info.distance);
+                    }
+                }
+            }
+        }
+    }
+}
