c95e11a6d3315bdcbc761481331ad43b30b00be3
William Taylor <willtay123@hotmail.co.uk>
2019-05-25 22:33:11 +0100

    fixed BuildSitePlacer (currently the ray only points down from the object using it)

--- Aura VR/Assets/Scripts/Building/BuildSitePlacer.cs
+++ Aura VR/Assets/Scripts/Building/BuildSitePlacer.cs
@@ -19,16 +19,17 @@ public class BuildSitePlacer : MonoBehaviour
 
     Ray _ray;
     RaycastHit _hit;
+    Vector3 _spawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        _buildSiteBeingPlaced = null;
         if (_placementIndicator != null)
         {
             _placementIndicator = Instantiate(_placementIndicator);
             _placementIndicator.SetActive(false);
         }
+        _ray = new Ray(transform.position, Vector3.down);
     }
 
     // Update is called once per frame
@@ -37,14 +38,17 @@ public class BuildSitePlacer : MonoBehaviour
         if (Input.GetKeyDown(placeKey))
         {
             placing = true;
-            Physics.Raycast(_ray, out _hit, 100, buildMask);
-            _placementIndicator.transform.position = _hit.point;
-            _placementIndicator.SetActive(true);
+            if (Physics.Raycast(_ray, out _hit, 100, buildMask))
+            {
+                _spawnPoint = _hit.point;
+                _placementIndicator.transform.position = _hit.point;
+                _placementIndicator.SetActive(true);
+            }
         }
         if (Input.GetKeyUp(placeKey) && placing)
         {
             placing = false;
-            Instantiate(_buildSiteBeingPlaced, _hit.point, Quaternion.identity);
+            Instantiate(_buildSiteBeingPlaced, _spawnPoint, Quaternion.identity);
             _placementIndicator.SetActive(false);
         }
     }    
