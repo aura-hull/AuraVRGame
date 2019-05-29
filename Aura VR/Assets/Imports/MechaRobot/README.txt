e537559a5d67abbb8157ac0ac672c6f3a767e31b
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-05-29 20:54:22 +0100

    Added Final IK and a script which auto-moves rigging targets based on the VRTK SDK selected rig. Also attached screen to arm.

---
+++ Aura VR/Assets/Imports/MechaRobot/README.txt
@@ -0,0 +1,10 @@
+Hi fellow developer,
+
+First of all, thank you very much for your purchase, you're the best! And all the best for you and your game project(s).
+I'd be glad if you add my name in the credits of the game that used my lovingly-crafted 3d character. :)
+Or email/twit me the game's website, Steam Greenlight page or whatever so I could help you guys. :)
+@dantemendes on twitter
+dante.mendes@gmail.com
+Paladin Dog Productions on Unity Asset Store.
+
+
---
+++ Aura VR/Assets/Scripts/Liam Wilson/IKTargetHandler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RootMotion.FinalIK;
+using UnityEngine;
+using VRTK;
+
+public class IKTargetHandler : MonoBehaviour
+{
+    private static Vector3 headTargetPosition = new Vector3(0, -0.07827437f, -0.1492147f);
+    private static Vector3 headTargetEuler = new Vector3(0, -90, -90);
+
+    private static Vector3 leftTargetPosition = new Vector3(-0.03f, -0.03f, -0.1f);
+    private static Vector3 leftTargetEuler = new Vector3(90, 0, -90);
+
+    private static Vector3 rightTargetPosition = new Vector3(0.03f, -0.03f, -0.1f);
+    private static Vector3 rightTargetEuler = new Vector3(-90, 0, 90);
+
+    [SerializeField] private VRTK_SDKManager vrtkManager;
+    [SerializeField] private VRIK finalIKSetup;
+
+    private GameObject headTarget = null;
+    private GameObject leftTarget = null;
+    private GameObject rightTarget = null;
+
+    void Awake()
+    {
+        VRTK_SDKManager.SubscribeLoadedSetupChanged(OnLoadedSetupChanged);
+    }
+
+    public void OnLoadedSetupChanged(VRTK_SDKManager sender, VRTK_SDKManager.LoadedSetupChangeEventArgs e)
+    {
+        // Instantiate IK targets.
+        VRTK_SDKSetup setup = sender.loadedSetup;
+        if (setup == null) return;
+
+        if (headTarget == null)
+        {
+            headTarget = new GameObject("HeadTarget");
+            headTarget.transform.SetParent(setup.actualHeadset.transform);
+            headTarget.transform.localPosition = headTargetPosition;
+            headTarget.transform.localEulerAngles = headTargetEuler;
+        }
+
+        if (leftTarget == null)
+        {
+            leftTarget = new GameObject("LeftTarget");
+            leftTarget.transform.SetParent(setup.actualLeftController.transform);
+            leftTarget.transform.localPosition = leftTargetPosition;
+            leftTarget.transform.localEulerAngles = leftTargetEuler;
+        }
+
+        if (rightTarget == null)
+        {
+            rightTarget = new GameObject("RightTarget");
+            rightTarget.transform.SetParent(setup.actualRightController.transform);
+            rightTarget.transform.localPosition = rightTargetPosition;
+            rightTarget.transform.localEulerAngles = rightTargetEuler;
+        }
+
+        // Set up IK links.
+        if (finalIKSetup == null) return;
+
+        finalIKSetup.solver.spine.headTarget = headTarget.transform;
+        finalIKSetup.solver.leftArm.target = leftTarget.transform;
+        finalIKSetup.solver.rightArm.target = rightTarget.transform;
+
+        //VRTK_SDKManager.UnsubscribeLoadedSetupChanged(OnLoadedSetupChanged);
+        //DestroyImmediate(this.gameObject);
+    }
+}
