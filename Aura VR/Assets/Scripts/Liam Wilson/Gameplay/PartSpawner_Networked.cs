b35e0ad03469b739a851e6afe884f03c4ed5f1ed
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-06-03 19:57:44 +0100

    Added a check for part destruction to spawn another.

--- Aura VR/Assets/Scripts/Liam Wilson/Gameplay/PartSpawner_Networked.cs
+++ Aura VR/Assets/Scripts/Liam Wilson/Gameplay/PartSpawner_Networked.cs
@@ -8,17 +8,17 @@ public class PartSpawner_Networked : MonoBehaviour
     [SerializeField] private Object partPrefab;
     [SerializeField] private Transform spawnPoint;
 
-    private bool isSpawned = false;
+    private PhotonView currentPart;
 
     public void SpawnIfReady()
     {
-        if (isSpawned) return;
+        if (currentPart != null) return;
         if (partPrefab == null) return;
 
         if (PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.Instantiate(partPrefab.name, spawnPoint.position, Quaternion.identity);
-            isSpawned = true;
+            GameObject partInstance = PhotonNetwork.Instantiate(partPrefab.name, spawnPoint.position, Quaternion.identity);
+            currentPart = partInstance.GetPhotonView();
         }
     }
 }
