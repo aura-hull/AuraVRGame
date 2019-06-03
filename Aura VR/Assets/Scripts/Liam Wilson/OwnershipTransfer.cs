d7b1eea0b489d32001843b30c36513247d293fd7
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-06-03 18:45:23 +0100

    Messing with ownership transfers

---
+++ Aura VR/Assets/Scripts/Liam Wilson/OwnershipTransfer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using VRTK;
+
+public class OwnershipTransfer : MonoBehaviour
+{
+    private PhotonView photonView;
+    private VRTK_InteractableObject vrtkObject;
+
+    public void Awake()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        vrtkObject = GetComponent<VRTK_InteractableObject>();
+        vrtkObject.InteractableObjectGrabbed += Transfer;
+    }
+
+    private void Transfer(object sender, InteractableObjectEventArgs e)
+    {
+        photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
+    }
+}
