6178f77b8667d855cfa739f193fb6eefcf2bea1e
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-06-13 23:43:41 +0100

    Removed unused assets and also changes water to simply scroll normal map across a plane.

---
+++ Aura VR/Assets/Scripts/Liam Wilson/ScrollNormals.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollNormals : MonoBehaviour
+{
+    [SerializeField] private Vector2 scrollSpeed = Vector2.one * 0.25f;
+
+    private Renderer _renderer;
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 currentOffset = _renderer.material.GetTextureOffset("_MainTex");
+        Vector2 newOffset = currentOffset + (scrollSpeed * Time.deltaTime);
+        _renderer.material.SetTextureOffset("_MainTex", newOffset);
+    }
+}
