fc14132b9aabf0817953aaa1bb1ed76ee7c0b881
William Taylor <willtay123@hotmail.co.uk>
2019-06-20 18:47:29 +0100

    added server content

---
+++ Aura VR/ScoreServer/README.txt
@@ -0,0 +1,15 @@
+To start the web page, call the below from CMD
+
+ 
+
+"C:\Program Files\IIS Express\iisexpress.exe" /path:[folderPath] /port:8080
+
+ 
+
+where [folderPath] is the path to the parent folder. E.g.
+"C:\Users\lucius laptop\Downloads\ScoreServer"
+
+ 
+
+
+The website can then be access through localhost:8080
---
+++ Aura VR/ScoreServer/main.js
@@ -0,0 +1,72 @@
+loadXMLDoc("scores.xml", 10);
+
+ 
+
+function loadXMLDoc(filepath, leaderboardSize) {
+    console.log("Attempting to load XML");
+    
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for older browsers
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            renderScores(this, "tblScores");
+        }
+    };
+    xmlhttp.open("GET", filepath, true);
+    xmlhttp.send();
+}
+
+ 
+
+function renderScores(xml, tableId) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    
+    var table="<tr><th class='rankHeader'>Rank</th>" +
+        "<th class='nameHeader'>Name</th>" +
+        "<th class='scoreHeader'>Score</th></tr>";
+    var x = xmlDoc.getElementsByTagName("score");
+    for (i = 0; i < 10; i++) {
+		var rank = i + 1;
+		var name = " ";
+		var score = " ";
+		
+		if (x.length > i) {
+			console.log("x : " + x.length);
+			console.log("i : " + i);
+			rank = x[i].getElementsByTagName("rank")[0].childNodes[0].nodeValue;
+			name = x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+			score = x[i].getElementsByTagName("value")[0].childNodes[0].nodeValue;
+		}
+        
+        console.log("Adding record for [" + name + "] with the score: " + score);
+        
+        var row = "";
+        row += "<tr>";
+        
+        // Rank
+        row += "<td class='rankCell'>";
+        row += rank;
+        row += "</td>"
+        
+        // Name
+        row += "<td class='nameCell'>";
+        row += name;
+        row += "</td>"
+        
+        // Score
+        row += "<td class='scoreCell'>";
+        row += score;
+        row += "</td>";
+        
+        row += "</tr>";
+        table += row;
+    }
+    document.getElementById(tableId).innerHTML = table;
+}
