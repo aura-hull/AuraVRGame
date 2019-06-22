0c77c4ea87a45bd88362c9c64f160d6381dfb7f6
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-06-23 00:10:30 +0100

    ?

---
+++ Aura VR/Assets/Scripts/Liam Wilson/TooltipVisibility.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipVisibility : MonoBehaviour
+{
+    [SerializeField] private GameObject triggerTooltip;
+    [SerializeField] private GameObject gripTooltip;
+}
