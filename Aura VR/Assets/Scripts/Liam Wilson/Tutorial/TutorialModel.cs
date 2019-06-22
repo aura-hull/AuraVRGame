1cfcc283134b28d95723f8cd6bc113458f45ccbc
Liam Wilson <liamryanwilson@hotmail.co.uk>
2019-06-22 12:00:59 +0100

    ...

--- Aura VR/Assets/Scripts/Liam Wilson/Tutorial/TutorialModel.cs
+++ Aura VR/Assets/Scripts/Liam Wilson/Tutorial/TutorialModel.cs
@@ -37,8 +37,6 @@ public class TutorialModel : MonoBehaviour
 
         NetworkController.OnTutorialClientReady += OnClientReady;
         NetworkController.OnPlayNextTutorial += SpeakWhenReady;
-
-        gameObject.SetActive(false);
     }
 
     public void Initialize()
